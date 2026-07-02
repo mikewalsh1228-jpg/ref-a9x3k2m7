b3d4f326076d483f6bc6c66c4901f6a70a68f669
mikewalsh1228-jpg <mikewalsh1228@yahoo.com>
2026-07-01 21:35:40 -0700

    phase 1-3: sources/links, half-life+storage, labs interpreter, interaction checker, favorites, theme, print, resources, deep-links, PWA offline install

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE='cref-v1';
+const ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png','./icon-180.png'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS).catch(()=>{})).then(()=>self.skipWaiting()));});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))).then(()=>self.clients.claim()));});
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request,{ignoreSearch:true}).then(h=>h||fetch(e.request).catch(()=>caches.match('./index.html'))));});
